aa89bcb7b596354b18238b05dbe0d0ceb0cfc608
Glenn Torrens <glenntorrens@gmail.com>
2025-07-23 21:14:01 +0900

    initiate resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import Navbar from "~/components/Navbar";
+import { resumes } from "~/constants/index";
+import ResumeCard from "~/components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "AI feedback for Stellar Resumes!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover  ">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Resume Ratings & Applications</h1>
+          <h2>
+            Resumind is a tool that helps you track your resume ratings and
+            applications.
+          </h2>
+        </div>
+      </section>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
